5e20c4436a57eb850a3524ad47850a5bfb2ff60e
jose garcia orozco <jose.garciaorozco@telefonica.com>
2021-04-08 16:21:41 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,8 +14,8 @@ let package = Package(
         .target(
             name: "Earth", 
             dependencies: [],
-            sources : "Sources/ios/"
+         //   sources : "Sources/ios/",
             resources: [
-                 .process("Resources")])),
+                 .process("Resources")]),
     ]
 )
